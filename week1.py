ef238d0b5ac0b2630f3a69a66bd4e6965ef38079
Richard Galvez <galvez@cuda1.cims.nyu.edu>
2018-04-02 14:46:59 -0400

    posting solution for week 1

---
+++ week1.py
@@ -0,0 +1,25 @@
+"""
+Problem:
+Write a program that prints the numbers from 1 to 100. But for multiples of 
+three print "Fizz" instead of the number and for the multiples of five print 
+"Buzz". For numbers which are multiples of both three and five print 
+"FizzBuzz".
+
+"""
+
+for k in range(1,101):
+
+    if (k%3==0) and (k%5==0):
+        print('FizzBuzz')
+
+    elif k % 3 == 0:
+        print('Fizz')
+
+    elif k % 5 == 0:
+        print('Buzz')
+
+    else:
+      print(k)
+
+
+
